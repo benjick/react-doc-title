c18ccb399f78f336359911e7128fd00cc920985f
Max Malm <benjick@dumfan.net>
2019-02-11 13:24:02 +0100

    Updates

---
+++ precache-manifest.5e9a01ae695fb6d92acfdf011dff5fcc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "abb95b79bd9ee79effef",
+    "url": "/static/js/main.abb95b79.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "676a00a250e2990babf6",
+    "url": "/static/js/2.676a00a2.chunk.js"
+  },
+  {
+    "revision": "6aba561cbc1653eeffb1d9182a3cda7f",
+    "url": "/index.html"
+  }
+];
